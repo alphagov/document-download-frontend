b104c489fb0b687870f362dc856bde7f7dbbac8e
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2019-10-23 13:04:05 +0100

    remove gunicorn server header
    see https://github.com/alphagov/notifications-api/pull/2640 for more
    information

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,5 @@
 import os
+import gunicorn
 
 bind = "0.0.0.0:{}".format(os.getenv("PORT"))
 
@@ -8,3 +9,4 @@ worker_class = "eventlet"
 worker_connections = 1000
 
 errorlog = "/home/vcap/logs/gunicorn_error.log"
+gunicorn.SERVER_SOFTWARE = ''
