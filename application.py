0794da50e925a13a8ac00fce3cea183bec81fd15
Samuel Williams <samuelhwilliams@gmail.com>
2023-05-03 11:51:03 +0100

    Initialise sentry

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
-import os
+from app.performance import init_performance_monitoring
 
-from flask import Flask
-from whitenoise import WhiteNoise
+init_performance_monitoring()
 
-from app import create_app
+import os  # noqa
+
+from flask import Flask  # noqa
+from whitenoise import WhiteNoise  # noqa
+
+from app import create_app  # noqa
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "app", "static")
