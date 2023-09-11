476aad3c9b47220c1d9c5c50f56f546f788f8cc6
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2023-09-11 11:29:15 +0100

    Tidy up spacing of gunicorn file
    So it matches the API equivalent
    https://github.com/alphagov/notifications-api/blob/main/gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,12 +2,9 @@ import os
 
 import gunicorn
 
-bind = "0.0.0.0:{}".format(os.getenv("PORT"))
-
 workers = 10
-
 worker_class = "eventlet"
 worker_connections = 1000
-
+bind = "0.0.0.0:{}".format(os.getenv("PORT"))
 errorlog = "/home/vcap/logs/gunicorn_error.log"
 gunicorn.SERVER_SOFTWARE = "None"
