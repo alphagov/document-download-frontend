412bbe0abe6160577903344a84cf53bdcd6f8c6f
Katie Smith <klssmith@users.noreply.github.com>
2023-12-07 14:47:33 +0000

    Use `BaseAPIClient` instead of `NotificationsAPIClient`
    We aren't using any of the methods the `NotificationsAPIClient` adds, so
    can just the `BaseAPIClient` that it inherits from directly.

--- app/notify_client/service_api_client.py
+++ app/notify_client/service_api_client.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from notifications_python_client.notifications import NotificationsAPIClient
+from notifications_python_client.notifications import BaseAPIClient
 
 
 class ServiceApiClient:
@@ -8,7 +8,7 @@ class ServiceApiClient:
         self.api_client = None
 
     def init_app(self, application):
-        self.api_client = NotificationsAPIClient(base_url=application.config["API_HOST_NAME"], api_key="a" * 75)
+        self.api_client = BaseAPIClient(base_url=application.config["API_HOST_NAME"], api_key="a" * 75)
         self.api_client.service_id = application.config["ADMIN_CLIENT_USER_NAME"]
         self.api_client.api_key = application.config["ADMIN_CLIENT_SECRET"]
 
