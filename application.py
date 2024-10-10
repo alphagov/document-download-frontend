c0092e958d57ed94976cf04458ce673c1d2ac92a
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-10-10 15:12:30 +0100

    application: use EventletTimeoutMiddleware if using eventlet
    also catch EventletTimeout exceptions, turning these into 504s,
    which are the closest thing to an application timeout standard
    codes seem to cover
    
    also control the timeout time with a
    `HTTP_SERVE_TIMEOUT_SECONDS` config/env var that we can control
    from e.g. terraform, unifying this with the value we use for
    gunicorn's `timeout` (which has little effect on the eventlet
    `worker_class`, but controls a similar behaviour on non-async
    workers), with the intention we start to respect this across all
    our apps.
    
    default to 30s timeout as this is what we've decided to set our
    cloudfront/ALB timeouts to (we'll make this change shortly)
    
    in EventletTimeoutMiddleware's current implementation, the
    timeout gets cancelled as soon as the initial status response
    is returned, so streaming responses shouldn't be affected any
    more than normal responses

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import os
+
 from app.performance import init_performance_monitoring
 
 init_performance_monitoring()
@@ -9,6 +11,8 @@ from whitenoise import WhiteNoise  # noqa
 
 from app import create_app  # noqa
 
+from notifications_utils.eventlet import EventletTimeoutMiddleware, using_eventlet  # noqa
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "app", "static")
 STATIC_URL = "static/"
@@ -17,3 +21,9 @@ application = Flask("app")
 
 create_app(application)
 application.wsgi_app = WhiteNoise(application.wsgi_app, STATIC_ROOT, STATIC_URL)
+
+if using_eventlet:
+    application.wsgi_app = EventletTimeoutMiddleware(
+        application.wsgi_app,
+        timeout_seconds=int(os.getenv("HTTP_SERVE_TIMEOUT_SECONDS", 30)),
+    )
