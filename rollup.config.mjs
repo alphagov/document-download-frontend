4ceabbd4bfb5a365f3e9760d51b98b6de35ce260
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-10-31 14:32:21 +0000

    Introduce a Rollup config
    Config contains two separate sections:
    
    1. ESM JavaScript bundle compilation
    2. SCSS compilation
    
    1. ESM JavaScript bundle compilation
    
    Single input to create
    a minified ESM bundle that contains links to
    sourcemaps.
    
    As Rollup "task" requires and input and output,
    and the copy plugin doesn't have separate ones,
    it's been added to this one.
    
    2. SCSS compilation
    
    Sadly, globbing isn't supported so we need to
    list the entry points we import.
    
    We minimise include links to sourcemaps and we
    disable the CSS URL resolver
    warnings (as referenced static files exists
    elsewhere). For production build we also rewrite
    the url to those assets as they're stored elsewhere.

---
+++ rollup.config.mjs
@@ -0,0 +1,59 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
+import styles from "rollup-plugin-styler";
+
+const paths = {
+  src: 'app/assets/',
+  dist: 'app/static/',
+  npm: 'node_modules/',
+  govuk_frontend: 'node_modules/govuk-frontend/dist/govuk/'
+};
+
+export default [
+  // ESM compilation and copy static assets
+  {
+    input: paths.src + 'javascripts/main.mjs',
+    output: {
+      dir: paths.dist + 'javascripts/',
+      entryFileNames: 'main.js',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins: [
+      nodeResolve(),
+      terser(),
+      // copy images, error pages and govuk-frontend static assets
+      copy({
+        targets: [
+          { src: [ paths.src + 'images/**/*', paths.govuk_frontend + 'assets/images/**/*' ], dest: paths.dist + 'images/' },
+          { src: paths.govuk_frontend + 'assets/fonts/**/*', dest: paths.dist + 'fonts/' },
+          { src: paths.govuk_frontend + 'assets/manifest.json', dest: paths.dist }
+        ]
+      }),
+    ]
+  },
+  // SCSS compilation
+  {
+    input: paths.src + 'stylesheets/main.scss',
+    output: {
+      dir: paths.dist + 'stylesheets/',
+      assetFileNames: "[name][extname]",
+    },
+    plugins: [
+      // https://anidetrix.github.io/rollup-plugin-styles/interfaces/types.Options.html
+      styles({
+        mode: "extract",
+        sass: {
+          includePaths: [
+            paths.govuk_frontend,
+            paths.npm
+          ],
+        },
+        minimize: true,
+        url: false,
+        sourceMap: true,
+      }),
+    ]
+  }
+];
