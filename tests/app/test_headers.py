bb2161c88da16a99904bf4152bab50aed3b52cbd
Will Pearson <will.pearson@digital.cabinet-office.gov.uk>
2026-01-29 16:00:38 +0000

    Fix ITHC security headers
    This adds the headers
    
    X-Content-Type-Options: X-Content-Type-Options: nosniff
    
    Strict-Transport-Security: Strict-Transport-Security: max-age=31536000; includeSubDomains
    
    Cache-control: Cache-control: no-store / Pragma: no-cache
    
    X-Frame-Options: X-Frame-Options: DENY / X-Frame-Options: SAMEORIGIN
    
    X-Permitted-Cross-Domain-Policies: X-Permitted-Cross-Domain-Policies: none
    
    It removes
    
    X-Xss-Protection: 1; mode=block

--- tests/app/test_headers.py
+++ tests/app/test_headers.py
@@ -25,9 +25,9 @@ def test_owasp_useful_headers_set(
     )
 
     assert response.headers["X-Robots-Tag"] == "noindex, nofollow"
-    assert response.headers["X-Frame-Options"] == "deny"
+    assert response.headers["X-Frame-Options"] == "DENY"
     assert response.headers["X-Content-Type-Options"] == "nosniff"
-    assert response.headers["X-XSS-Protection"] == "1; mode=block"
+    assert response.headers["X-Permitted-Cross-Domain-Policies"] == "none"
     assert response.headers["Content-Security-Policy"] == (
         "default-src 'self';"
         "script-src 'self' 'nonce-TESTs5Vr8v3jgRYLoQuVwA';"
@@ -40,8 +40,10 @@ def test_owasp_useful_headers_set(
         "frame-src 'self';"
     )
 
-    assert response.headers["Strict-Transport-Security"] == "max-age=31536000; preload"
+    assert response.headers["Strict-Transport-Security"] == "max-age=31536000; includeSubDomains"
     assert response.headers["Referrer-Policy"] == "no-referrer"
+    assert response.headers["Cache-Control"] == "no-store, no-cache, private, must-revalidate"
+    assert response.headers["Pragma"] == "no-cache"
     assert response.headers["Cross-Origin-Embedder-Policy"] == "require-corp;"
     assert response.headers["Cross-Origin-Opener-Policy"] == "same-origin;"
     assert response.headers["Cross-Origin-Resource-Policy"] == "same-origin;"
